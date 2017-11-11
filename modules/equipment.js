88195eba5cdf1f90da5478f6b3ae4e319ca5cfbc
AzzerUK <11813334+AzzerUK@users.noreply.github.com>
2017-11-11 13:02:18 +0000

    Update equipment.js

--- modules/equipment.js
+++ modules/equipment.js
@@ -196,12 +196,12 @@ function evaluateEquipmentEfficiency(equipName) {
     }
     // Don't go above our main equipment cap OR double equipment cap for spires
     var iCapLevel = getPageSetting('CapEquip2');
-    debug('World: ' + game.global.world + ' IgnoreSpires: ' + getPageSetting('IgnoreSpiresUntil') + ' spire active: ' + game.global.spireActive);
+    debug('World: ' + game.global.world + ' IgnoreSpires: ' + getPageSetting('IgnoreSpiresUntil') + ' spire active: ' + game.global.spireActive + ', level cap: ' + iCapLevel);
     if (game.global.world >= getPageSetting('IgnoreSpiresUntil') && (game.global.world == 200 || game.global.world == 300 || game.global.world == 400 || game.global.world == 500 || game.global.world == 600) && game.global.spireActive) {
         iCapLevel *= 2;
-        debug('Doubling cap level!?');
+        debug('Doubling cap level to: ' + iCapLevel);
     }
-    debug('CapLevel: ' + iCapLevel + ' ThisLevel: ' + gameResource.level);
+    debug('CapLevel now at: ' + iCapLevel + ' ThisLevel: ' + gameResource.level);
     if (iCapLevel > 0 && gameResource.level >= iCapLevel) {
         Factor = 0;
         Wall = true;
